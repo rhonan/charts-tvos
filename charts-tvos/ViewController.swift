a54292f99d3e1530b49c7754f09881221fd4d27a
Rhonan Carneiro <rhonanc@gmail.com>
2015-11-27 13:43:15 -0200

    created NSTimer to update the chart.

--- charts-tvos/ViewController.swift
+++ charts-tvos/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
         let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
         
         setChart(months, values: unitsSold)
-        
+        NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: Selector("sayHello"), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,6 +43,13 @@ class ViewController: UIViewController {
         let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
         barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
         barChartView.data = chartData
+    }
+    
+    func sayHello()
+    {
+        let unitsSold = [20.0, 20.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 8.0]
+        setChart(months, values: unitsSold)
+        NSLog("Chart updated.")
     }
 }
 
