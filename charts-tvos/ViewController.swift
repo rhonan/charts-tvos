5a44ec5975a6c7d0961c54d47eae488237c952d4
Fernando Madruga Pinheiro <fernando.pinheiro@gmail.com>
2015-11-27 14:35:09 -0200

    Adicionado gráfico de pizza.

--- charts-tvos/ViewController.swift
+++ charts-tvos/ViewController.swift
@@ -12,16 +12,15 @@ import Charts
 class ViewController: UIViewController {
 
     @IBOutlet weak var barChartView: BarChartView!
-    
-    var months: [String]!
+    @IBOutlet weak var pieChartView: PieChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
         
-        setChart(months, values: unitsSold)
+        createBarChart()
+        createPieChart()
+        
         NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: Selector("updateCharts"), userInfo: nil, repeats: true)
     }
 
@@ -30,29 +29,60 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func setChart(dataPoints: [String], values: [Double]) {
+    func createBarChart() {
         barChartView.noDataText = "Não há dados para serem exibidos."
+        barChartView.descriptionText = "Novos clientes por mês"
+        barChartView.descriptionFont = UIFont(name: "Arial", size: 25)
+        barChartView.descriptionTextPosition = CGPoint(x: 400, y: 2)
+
+        let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
         var dataEntries: [BarChartDataEntry] = []
         
-        for i in 0..<dataPoints.count {
-            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+        for i in 0..<months.count {
+            let dataEntry = BarChartDataEntry(value: unitsSold[i], xIndex: i)
             dataEntries.append(dataEntry)
         }
         
         let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
-        let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
+        chartDataSet.colors = ChartColorTemplates.colorful()
+
         barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0)
-        barChartView.data = chartData
+        barChartView.data = BarChartData(xVals: months, dataSet: chartDataSet)
     }
     
-    func updateCharts()
-    {
+    func createPieChart() {
+        pieChartView.noDataText = "Não há dados para serem exibidos."
+        pieChartView.descriptionText = "Eventos em aberto no momento"
+        pieChartView.descriptionFont = UIFont(name: "Arial", size: 25)
+        pieChartView.descriptionTextPosition = CGPoint(x: 400, y: 2)
         
-        barChartView.data?.dataSets[0].entryForXIndex(1)?.value = (barChartView.data?.dataSets[0].entryForXIndex(1)?.value )! + 1
+        let types = ["Alarme", "Arme", "Desarme", "Pânico", "Sem controle"]
+        let qty = [20.0, 90.0, 10.0, 34.0, 12.0]
         
+        var dataEntries: [ChartDataEntry] = []
+        for i in 0..<types.count {
+            let dataEntry = ChartDataEntry(value: qty[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = PieChartDataSet(yVals: dataEntries, label: "Eventos")
+        chartDataSet.colors = ChartColorTemplates.colorful()
+
+        pieChartView.data = PieChartData(xVals: types, dataSet: chartDataSet);
+    }
+    
+    func updateCharts() {
+        // bar chart
+        barChartView.data?.dataSets[0].entryForXIndex(1)?.value = (barChartView.data?.dataSets[0].entryForXIndex(1)?.value)! + 1
         barChartView.notifyDataSetChanged()
         
-        NSLog("Chart updated.")
+        // line chart
+        pieChartView.data?.dataSets[0].entryForXIndex(1)?.value = (pieChartView.data?.dataSets[0].entryForXIndex(1)?.value)! + 1
+        pieChartView.notifyDataSetChanged()
+        
+        NSLog("Charts updated.")
     }
 }
 
