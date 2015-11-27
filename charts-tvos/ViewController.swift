ec26f9d943f380cdedab119236ae3cfd65c43ff6
Rhonan Carneiro <rhonanc@gmail.com>
2015-11-27 10:26:33 -0200

    first static chart added.

--- charts-tvos/ViewController.swift
+++ charts-tvos/ViewController.swift
@@ -11,9 +11,18 @@ import Charts
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var barChartView: BarChartView!
+    
+    var months: [String]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        setChart(months, values: unitsSold)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +30,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setChart(dataPoints: [String], values: [Double]) {
+        barChartView.noDataText = "Não há dados para serem exibidos."
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
+        barChartView.data = chartData
+    }
 }
 
